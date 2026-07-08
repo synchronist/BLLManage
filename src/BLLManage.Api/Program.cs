03f866b969dffbdd0fe8bfdd2c71b755a7249ba9
Fillipy <lip-crs@hotmail.com>
2026-07-08 04:31:05 -0300

    refactor: cleaning api bootstrap and enabling controllers

---
+++ src/BLLManage.Api/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
